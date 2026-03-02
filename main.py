e0e38df76ceaba465418a2066c9ec423a0e33ce0
eric-arita <eric.soares.arita@gmail.com>
2026-03-02 18:00:19 -0300

    Implementado Ollama

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+
 
 load_dotenv()
 
 def main():
-    print("Hello from langchain-course!")
+    print("Hello!")
 
-    information = """Elon Reeve Musk ( born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, Twitter, and xAI. Musk has been the wealthiest person in the world since 2025; as of February 2026, Forbes estimates his net worth to be around US$852 billion.
+    information = """Alex Sandro Silva Pereira[1] (born 7 July 1987) is a Brazilian professional mixed martial artist and former professional kickboxer. He currently competes in the Light Heavyweight division of the Ultimate Fighting Championship (UFC), where he is the current and two-time UFC Light Heavyweight Champion and former UFC Middleweight Champion. As of 3 February 2026, he is #5 in the UFC men's pound-for-pound rankings.[8]
 
-Born into a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he has Canadian citizenship since his mother was born there. He received bachelor's degrees in 1997 from the University of Pennsylvania before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. Musk also became an American citizen in 2002.
-
-In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence (AI) research, but later left; growing discontent with the organization's direction and their leadership in the AI boom in the 2020s led him to establish xAI, which became a subsidiary of SpaceX in 2026. In 2022, he acquired the social network Twitter, implementing significant changes, and rebranding it as X in 2023. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017. In November 2025, a Tesla pay package worth $1 trillion for Musk was approved, which he is to receive over 10 years if he meets specific goals.
-
-Musk was the largest donor in the 2024 U.S. presidential election, where he supported Donald Trump. After Trump was inaugurated as president in early 2025, Musk served as Senior Advisor to the President and as the de facto head of the Department of Government Efficiency (DOGE). After a public feud with Trump, Musk left the Trump administration and returned to managing his companies. Musk is a supporter of global far-right figures, causes, and political parties. His political activities, views, and statements have made him a polarizing figure. Musk has been criticized for COVID-19 misinformation, promoting conspiracy theories, and affirming antisemitic, racist, and transphobic comments. His acquisition of Twitter was controversial due to a subsequent increase in hate speech and the spread of misinformation on the service, following his pledge to decrease censorship. His role in the second Trump administration attracted public backlash, particularly in response to DOGE. The emails he sent to Jeffrey Epstein are included in the Epstein files, which were published between 2025–26 and became a topic of worldwide debate
+He is the ninth fighter in UFC history to become champion in two different weight divisions and the first to become champion in both the middleweight and light heavyweight divisions of the organization. In kickboxing, he is a former Glory middleweight and light heavyweight champion, and is the first and only fighter to have held Glory titles in two weight classes simultaneously. Pereira also competed in promotions such as It's Showtime and Superkombat Fighting Championship in kickboxing, and for Jungle Fight and Legacy Fighting Alliance in MMA. Pereira is the only known fighter to be a two-division world champion in MMA and kickboxing and is regarded as one of the greatest combat athletes of all time.[9][10][11][12][13] Pereira was ranked #1 in the kickboxing middleweight and light-heavyweight rankings in February
 """
     summary_template = """
     given the information {information} about a person I want you create:
@@ -25,7 +23,8 @@ Musk was the largest donor in the 2024 U.S. presidential election, where he supp
         input_variables = ['infomation'], template = summary_template
     )
 
-    llm = ChatOpenAI(temperature = 0, model = "gpt-5")
+    #llm = ChatOpenAI(temperature = 0, model = "gpt-5")
+    llm = ChatOllama(temperature =0, model="gemma3:270m")
     chain = summary_prompt_template | llm
     response = chain.invoke(input={"information": information})
     print(response.content)
